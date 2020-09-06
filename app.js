89e50de0e4a7ecd62298647508c23f47b9200250
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-09-06 12:17:32 +0530

    prototype designed

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 var box = document.querySelectorAll(".col-2")
 var height = document.querySelectorAll(".col-2")[0].clientWidth;
 console.log(box)
-console.log(height)
+
 
 box.forEach((ele)=>{
     ele.style.height = `${height}px`;
 })
 box.forEach((ele)=>{
-    ele.style.backgroundColor = "#505050";
+    ele.style.backgroundColor = "#fff";
 })
 
+var container = document.getElementsByClassName('container-main')[0];
+var questionBox = document.getElementsByClassName('question-box')[0];
+var close = document.getElementsByClassName("close")[0].addEventListener('click',()=>{
+    container.style.display = "block";
+    questionBox.style.display = "none";
+});
 box.forEach((ele)=>{
     ele.addEventListener("click",(e)=>{
-        e.target.style.backgroundColor = "";
+        console.log(e.target.id)
+        // e.target.style.backgroundColor = "";
+        console.log(container)
+        container.style.display = "none";
+        console.log(questionBox)
+        questionBox.style.display = "block";
+
+
     })
 })
 
