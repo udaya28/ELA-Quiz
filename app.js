de22a539ca85152e77ed10b4cfa904e798f9d46a
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-09-06 15:06:52 +0530

    completed working

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+console.log(data);
 var box = document.querySelectorAll('.col-2');
 var height = document.querySelectorAll('.col-2')[0].clientWidth;
 console.log(box);
@@ -18,23 +19,47 @@ var close = document
     container.style.display = 'block';
     header.style.display = 'block';
     questionBox.style.display = 'none';
-
   });
 box.forEach((ele) => {
   ele.addEventListener('click', (e) => {
     console.log(e.target.id);
-    // e.target.style.backgroundColor = "";
-    console.log(container);
+    let questionNumber = e.target.id;
+    let question = data[questionNumber];
+    console.log(question);
+    document.getElementById('question').innerHTML = question['question'];
+    document.getElementById('op-1').innerHTML = question[1];
+    document.getElementById('op-2').innerHTML = question[2];
+    document.getElementById('op-3').innerHTML = question[3];
+    document.getElementById('op-4').innerHTML = question[4];
+
     container.style.display = 'none';
-    console.log(questionBox);
     questionBox.style.display = 'block';
     header.style.display = 'none';
-
+    document.getElementById('question-number').innerHTML = questionNumber;
     document.getElementsByClassName('okay')[0].addEventListener('click', () => {
-      e.target.style.backgroundColor = '';
-      container.style.display = 'block';
-      questionBox.style.display = 'none';
-      header.style.display = 'block';
+      let result = document.querySelector('input[name="answer"]:checked').value;
+      console.log(result);
+      if (result == question['ans']) {
+        swal({
+          title: 'Correct',
+          icon: 'success',
+        }).then(() => {
+            e.target.style.backgroundColor = '';
+            container.style.display = 'block';
+            questionBox.style.display = 'none';
+            header.style.display = 'block';
+          });
+      } else {
+        swal({
+          title: 'Wrong',
+          icon: 'error',
+        }).then(() => {
+          e.target.style.backgroundColor = '';
+          container.style.display = 'block';
+          questionBox.style.display = 'none';
+          header.style.display = 'block';
+        });
+      }
     });
   });
 });
