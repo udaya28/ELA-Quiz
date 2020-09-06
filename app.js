a515f61c00fd2271c3a58761141f5b9425719607
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-09-06 13:23:09 +0530

    design improvment and basic working

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
-var box = document.querySelectorAll(".col-2")
-var height = document.querySelectorAll(".col-2")[0].clientWidth;
-console.log(box)
+var box = document.querySelectorAll('.col-2');
+var height = document.querySelectorAll('.col-2')[0].clientWidth;
+console.log(box);
 
-
-box.forEach((ele)=>{
-    ele.style.height = `${height}px`;
-})
-box.forEach((ele)=>{
-    ele.style.backgroundColor = "#fff";
-})
+box.forEach((ele) => {
+  ele.style.height = `${height}px`;
+});
+box.forEach((ele) => {
+  ele.style.backgroundColor = '#252C4A';
+});
 
 var container = document.getElementsByClassName('container-main')[0];
 var questionBox = document.getElementsByClassName('question-box')[0];
-var close = document.getElementsByClassName("close")[0].addEventListener('click',()=>{
-    container.style.display = "block";
-    questionBox.style.display = "none";
+var close = document
+  .getElementsByClassName('close')[0]
+  .addEventListener('click', () => {
+    container.style.display = 'block';
+    questionBox.style.display = 'none';
+  });
+box.forEach((ele) => {
+  ele.addEventListener('click', (e) => {
+    console.log(e.target.id);
+    // e.target.style.backgroundColor = "";
+    console.log(container);
+    container.style.display = 'none';
+    console.log(questionBox);
+    questionBox.style.display = 'block';
+    document.getElementsByClassName('okay')[0].addEventListener('click', () => {
+      e.target.style.backgroundColor = '';
+      container.style.display = 'block';
+      questionBox.style.display = 'none';
+    });
+  });
 });
-box.forEach((ele)=>{
-    ele.addEventListener("click",(e)=>{
-        console.log(e.target.id)
-        // e.target.style.backgroundColor = "";
-        console.log(container)
-        container.style.display = "none";
-        console.log(questionBox)
-        questionBox.style.display = "block";
-
-
-    })
-})
-
